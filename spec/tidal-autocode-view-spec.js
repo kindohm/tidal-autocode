ee3f5d1e906dd45374a19cd11ef54c75346b4a8b
Mike Hodnick <mike.hodnick@gmail.com>
2016-04-26 15:16:50 -0500

    first

---
+++ spec/tidal-autocode-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TidalAutocodeView from '../lib/tidal-autocode-view';
+
+describe('TidalAutocodeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
